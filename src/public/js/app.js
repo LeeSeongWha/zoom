d621fa1316a94f6010972a3d0cd06beedbfeeb8e
leeseongwha <fpdlcm0614@gmail.com>
2023-07-19 19:03:06 +0900

    lee

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -10,6 +10,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
   try {
@@ -124,6 +125,11 @@ welcomeForm.addEventListener('submit', handleWelcomeSubmit);
 
 // Peer A
 socket.on('welcome', async () => {
+  myDataChannel = myPeerConnection.createDataChannel('chat');
+  myDataChannel.addEventListener('message', (event) => {
+    console.log(event.data);
+  });
+  console.log('made data channel');
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
   console.log('sent the offer');
@@ -132,6 +138,12 @@ socket.on('welcome', async () => {
 
 // Peer B
 socket.on('offer', async (offer) => {
+  myPeerConnection.addEventListener('datachannel', (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener('message', (event) => {
+      console.log(event.data);
+    });
+  });
   console.log('received answer');
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
@@ -152,7 +164,19 @@ socket.on('ice', (ice) => {
 // RTC Code
 
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+          'stun:stun3.l.google.com:19302',
+          'stun:stun4.l.google.com:19302',
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener('icecandidate', handleIce);
   myPeerConnection.addEventListener('addstream', handleAddStream);
   myStream
